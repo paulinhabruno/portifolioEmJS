4986e299414127a3517549038b76ee4eddfefe0d
paulinhabruno <mapalurj@gmail.com>
2022-03-07 14:46:48 -0300

    inicio logica js da urna

---
+++ urna/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numero: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                nome: 'Fulado de Tal',
+                partido: 'ABC',
+                fotos: [
+                    { url: 'ver1.png', legenda: 'Vereador'}
+                ]
+            },
+            { 
+                numero: 77222,
+                nome: 'Beltrano de Tal',
+                partido: 'DEFG',
+                fotos: [
+                    { url: 'ver2.png', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    { 
+        titulo: 'PREFEITO',
+        numero: 2,
+        candidatos: [
+            { 
+                numero: 99,
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos: [
+                    { url: 'op1.jpg', legenda: 'Prefeito'},
+                    { url: 'vice1.png', legenda: 'Vice-prefeito', small: true}
+                ]
+            },
+            { 
+                numero: 84,
+                nome: 'Zilano',
+                partido: 'DEFG',
+                vice: 'Zul',
+                fotos: [
+                    { url: 'op2.png', legenda: 'Prefeito'},
+                    { url: 'vice2.png', legenda: 'Vice-prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
--- urna/scriptUrna.js
+++ urna/scriptUrna.js
@@ -8,3 +8,32 @@ for (let i=0; i< msg.length; i++) {
     })(i);
 };
 
+
+let seuVotoPara = document.querySelector('.d-1-1 spam');
+
+let cargo = document.querySelector('.d-1-2 spam');
+
+let descricao = document.querySelector('.d-1-4');
+
+let aviso = document.querySelector('.d-2');
+
+let lateral = document.querySelector('.d-1-right');
+
+let numeros = document.querySelector('.d-1-3');
+
+let etapaAtual = 0;
+
+
+function clicou(n) {
+    alert("Clicou em" +n);
+}
+
+function branco(){
+    alert("Clicou em BRANCO!");
+}
+function corrige(){
+    alert("Clicou em CORRIGE!");
+}
+function confirma(){
+    alert("Clicou em CONFIRMA!");
+}
